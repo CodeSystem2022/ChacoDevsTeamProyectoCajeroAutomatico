ff9dd83cc74263f92c7dd00129762f97b204ace5
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2022-10-23 16:34:10 -0300

    CDT-11-(Deriva del ticket CDT-2) se requiere que se implemente un metodo de opcion de seleccion del usuario

--- src/test/PruebaCajeroAutomatico.java
+++ src/test/PruebaCajeroAutomatico.java
@@ -1,19 +1,38 @@
 package test;
+import components.ValidaIngresoMenu;
+
 import model.CtaBancaria;
 import model.Domicilio;
 import model.Persona;
 import ui.*;
+
 public class PruebaCajeroAutomatico {
+    //Creacion de objetos con sus atributos para realizar pruebas
+    private static MenuPantallaPrincipalUI menuPantallaPrincipalUI = new MenuPantallaPrincipalUI();
+    private static Domicilio domicilio = new Domicilio("Barrio Guiraldes manzana1","Chaco","Resistencia");
+    private static Persona persona = new Persona("Espinola","Renzo",domicilio,"33074277");
+    private static CtaBancaria ctaBancaria = new CtaBancaria(persona,333225,5000,"Caja de Ahorro");
+
+
     public static void main(String[] args) {
-        Domicilio domicilio = new Domicilio("Barrio Guiraldes manzana1","Chaco","Resistencia");
-        Persona persona = new Persona("Espinola","Renzo",domicilio,"33074277");
-        CtaBancaria ctaBancaria = new CtaBancaria(persona,333225,5000,"Caja de Ahorro");
-
-        MenuPantallaPrincipalUI menuPantallaPrincipalUI = new MenuPantallaPrincipalUI();
         int response= menuPantallaPrincipalUI.pantallaPrincipalOpciones(ctaBancaria);
-
+        opcionSeleccion(response);
     }
-    public void opcionesSelecionadas(int opcion){
-
+    public static void opcionSeleccion(int opcion) {
+        ValidaIngresoMenu validaIngresoMenu = new ValidaIngresoMenu();
+        while (validaIngresoMenu.validaIngMenuPrin(opcion)) {
+           switch(opcion) {
+               case 1:
+                   break;
+               case 2:
+                   break;
+               case 3:
+                   break;
+               case 4:
+                   break;
+               case 5:SubMenuPantallaMovCons.pantallaSubMenuMovconsultas(ctaBancaria);
+               break;
+           }
+        }
     }
 }
