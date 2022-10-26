771b383fe67e85ba13f3e175f54a3797cb2177c4
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2022-10-26 15:40:10 -0300

    Prestamos
    Se agregó clase prestamos

---
+++ src/ui/prestamos/SubPantallaPrestamos.java
@@ -0,0 +1,30 @@
+package ui.prestamos;
+
+import model.CtaBancaria;
+
+public class SubPantallaPrestamos {
+  
+    public static void SubPantallaPrestamos(){
+
+    }
+}
+
+
+
+
+
+//	Escribir "              SOLICITUD DE PRESTAMOS                       ";
+//	Escribir "";
+//	Escribir "          INGRESE EL NRO. DE OPCIÓN DEL";
+//	Escribir "          PRESTAMO QUE DESEA SOLICITAR";
+//	Escribir "";
+//	Escribir "            OPCIÓN           IMPORTE($)";
+//	Escribir "              01               5.000";
+//	Escribir "              02               4.000";
+//	Escribir "              03               3.000";
+//	Escribir "              04               2.000";
+//	Escribir "              05               1.500";
+//	Escribir "              06               1.000";
+//	Escribir "";
+//	Escribir "              07<------------MODIFICAR";
+//	Escribir "              00<---------------VOLVER";
