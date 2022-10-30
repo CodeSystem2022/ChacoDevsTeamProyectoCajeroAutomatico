59a438f22b007e44f861d472a6aa5799aa1ed6bf
Juan Valladares <juan_1@outlook.es>
2022-10-30 20:52:10 -0300

    Update SubPantallaTransferencias

--- src/ui/transferencia/SubPantallaTransferencias.java
+++ src/ui/transferencia/SubPantallaTransferencias.java
@@ -15,26 +15,3 @@ public class SubPantallaTransferencias {
         );
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//Se requiere crear la pantalla con la siguiente estructura:
-//
-//         TRANSFERENCIAS/DEPOSITOS
-//Indique la cuenta de origen desde la cual desea transferir
-// 01-CA$ 1019584
-// 00-VOLVER
-//
-//Donde 01-CA$ 1019584 debe ser compuesto por el tipo de cuenta (getTipoCuentaBancaria()) del objeto CtaBancaria y su numero tambien compuesto por
-//el atributo numero de cuenta  (getNumCta()).
-
-//Este debe ser un metodo VOID y debe recibir como parametro el objeto CtaBancaria y debe estar dentro de un paquete en UI->transferencia
