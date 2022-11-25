f3aeba3c9b5884a5daddf43ae8f74069e183142e
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2022-11-25 15:06:02 -0300

    CDT-47-Se agregaron validaciones menu pagosrecargas

--- src/ui/transferencia/MenuTransferencia.java
+++ src/ui/transferencia/MenuTransferencia.java
@@ -2,20 +2,33 @@ package ui.transferencia;
 
 import components.Validaciones;
 import model.CtaBancaria;
+import model.TitulosPantallas;
 import ui.movimientosConsultas.SubPantallasMovimientosConsultas;
 
 import javax.swing.*;
 
 public class MenuTransferencia {
+    private Validaciones validaciones = new Validaciones();
     public void pantallaSubMenuMovconsultas(CtaBancaria ctaBancaria) {
-        int opcion = Integer.parseInt(JOptionPane.showInputDialog(new StringBuilder()
+        String opcion="0";
+        boolean bandera = false;
+        do {
+        opcion = (JOptionPane.showInputDialog(new StringBuilder()
                 .append("                   TRANSFERENCIAS/DEPOSITOS              \n")
                 .append("                     \n")
                 .append("\n").append("         SELECCIONE EL TIPO DE OPERACION    \n")
                 .append("1<--------TRANSFERENCIAS          \n")
                 .append("0<--------VOLVER                                  ")
                 .toString()));
-        opcionSeleccion(opcion, ctaBancaria);
+            opcion=opcion==null?"0":opcion;
+            if(opcion.equals("0")||validaciones.esUnNumero(opcion)) {
+                bandera = true;
+            }else {
+                JOptionPane.showMessageDialog(null, "ERROR, INGRESO INCORRECTO ", TitulosPantallas.TITULOMOVIMIENTOCONSULTAS.descripcion, JOptionPane.ERROR_MESSAGE);
+                bandera = false;
+            }
+        }while(!bandera);
+        opcionSeleccion(Integer.parseInt(opcion), ctaBancaria);
     }
 
     public void opcionSeleccion(int opcion, CtaBancaria ctaBancaria) {
